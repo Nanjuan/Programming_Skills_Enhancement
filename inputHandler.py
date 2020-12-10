fda0f4419112817a5aaec01ae21012ce5500eb6a
N4n_|u4n <ntnanjuan062@gmail.com>
2020-12-10 15:02:48 -0500

    updated file

--- inputHandler.py
+++ inputHandler.py
@@ -3,6 +3,7 @@
 # We need import appropiate library
 import xlrd
 import sys
+#import datetime
 
 #Variable Section 
 loc = ("C:\Tools\Programming Skills Enhancement\inputData.xlsx")
@@ -10,17 +11,29 @@ loc = ("C:\Tools\Programming Skills Enhancement\inputData.xlsx")
 #First open the excel file 
 #Second Read the content of the excel file one line at a time 
 def openExcel():
+
+
     wb = xlrd.open_workbook(loc)
-    print(wb)
     sheet = wb.sheet_by_index(0)
-    print(sheet)
-    print(sheet.nrows)
+    l = []
+    init = 
+
+    for i in range(sheet.nrows):
+
+        l.append(init + ())
+        print(l)
+        #print(sheet.cell_value(i, 0))
+        #print(sheet.row_values(i))
+        #print(sheet.cell_value(0,1))
+    
+
+
 #New Function for validation of data 
 
 
 def main():
     openExcel()
-    print("hello world!!")
+    #print("hello world!!")
     
 
 
