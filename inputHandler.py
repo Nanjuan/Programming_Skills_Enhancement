37caacb4e78553776393f70f9cfdfe5115f96c63
N4n_|u4n <ntnanjuan062@gmail.com>
2020-12-11 15:29:01 -0500

    started working on class

--- inputHandler.py
+++ inputHandler.py
@@ -12,17 +12,15 @@ loc = ("C:\Tools\Programming Skills Enhancement\inputData.xlsx")
 #Second Read the content of the excel file one line at a time 
 def openExcel():
 
-
     wb = xlrd.open_workbook(loc)
     sheet = wb.sheet_by_index(0)
-    l = []
-    init = 
+    l = [] #Ths is the list where the values from each row get store
 
     for i in range(sheet.nrows):
-
-        l.append(init + ())
-        print(l)
-        #print(sheet.cell_value(i, 0))
+        l.append(sheet.row_values(i))
+    
+    print(l)
+        #print(sheet.cell_value(i, 2))
         #print(sheet.row_values(i))
         #print(sheet.cell_value(0,1))
     
