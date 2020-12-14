61f16707f826c8f40685d83c3e17e4911c795ea9
N4n_|u4n <ntnanjuan062@gmail.com>
2020-12-14 17:18:27 -0500

    work on changing variable

--- excelRow.py
+++ excelRow.py
@@ -1,1 +1,11 @@
-Class excelRow :
+class excelRow:
+    def __init__(self, excelNum, excelDate, excelTitle, excelComment, excelPriority, excelStatus):
+        self.excelNum = excelNum
+        self.excelDate = excelDate
+        self.excelTitle = excelTitle
+        self.excelComment = excelComment
+        self.excelPriority = excelPriority
+        self.excelStatus = excelStatus
+    
+    def __repr__(self):
+        return str(self.__dict__)
--- inputHandler.py
+++ inputHandler.py
@@ -5,6 +5,8 @@ import xlrd
 import sys
 #import datetime
 
+from excelRow import excelRow
+
 #Variable Section 
 loc = ("C:\Tools\Programming Skills Enhancement\inputData.xlsx")
 
@@ -14,15 +16,29 @@ def openExcel():
 
     wb = xlrd.open_workbook(loc)
     sheet = wb.sheet_by_index(0)
+    rows = sheet.nrows
+    cols = sheet.ncols
+    
+    #print(sheet.row_values(0))
+    
     l = [] #Ths is the list where the values from each row get store
 
-    for i in range(sheet.nrows):
-        l.append(sheet.row_values(i))
-    
+    for i in range(rows):
+        excelNum = sheet.cell_value(i,0)
+        excelDate = sheet.cell_value(i,1)
+        excelTitle = sheet.cell_value(i,2)
+        excelComment = sheet.cell_value(i,3)
+        excelPriority = sheet.cell_value(i,4)
+        excelStatus = sheet.cell_value(i,5)
+        #excelCorrectDate = somethignfixday(excelDate)
+
+        excelObjRow = excelRow(excelNum, excelDate, excelTitle, excelComment, excelPriority, excelStatus)
+        l.append(excelObjRow)        
+
     print(l)
         #print(sheet.cell_value(i, 2))
         #print(sheet.row_values(i))
-        #print(sheet.cell_value(0,1))
+        #print(sheet.cell_value(0,0))
     
 
 
@@ -35,6 +51,5 @@ def main():
     
 
 
-if __name__ == '__main__':
-    
+if __name__ == '__main__':   
     main()
