64dbcfb29070087fac78a0f01698fe0835dc2e1e
Nestor Torres <ntnanjuan062@gmail.com>
2021-01-06 01:23:35 -0500

    added the node python small program

---
+++ python-app/Python-Update/updateHandler.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# We need import appropiate library
+import sqlite3
+
+conn = sqlite3.connect("dataStored.db")
+
+def ingestExcelTo(TaskList):
+    for myTask in TaskList:
+        conn.execute("""INSERT INTO
+                TaskList (rowNum, dateTaskEnter, dueDate, title, comments, priority, completion, status)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", (myTask.excelNum, myTask.excelDate, myTask.excelDueDate, myTask.excelTitle, myTask.excelComment, myTask.excelPriority, myTask.excelCompletion, myTask.excelStatus))
+
+    conn.commit()
---
+++ python-app/nodePythonApp/script1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from sys import argv
+
+term = argv[1]
+second = argv[2]
+
+print(term, second)
