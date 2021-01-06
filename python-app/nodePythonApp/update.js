5330f0dc22942c2def4cadf0f472d1b01e553291
Nestor Torres <ntnanjuan062@gmail.com>
2021-01-06 08:29:09 -0500

    sateted post node

---
+++ python-app/nodePythonApp/update.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const {spawn} = require('child_process');
+const app = express()
+const port = 3000
+// app.post('/', (req, res) =>
+app.post('/', (req, res) => {
+ 
+var dataToSend;
+//  var term = req.query.term; 
+//  var second = req.query.second;
+var third = req.body.term;
+// spawn new child process to call the python script
+// const python = spawn('python', ['script1.py']);
+ const python = spawn('python', ['script1.py', term]);
+ // collect data from script
+ python.stdout.on('data', function (data) {
+  console.log('Pipe data from python script ...');
+  dataToSend = data.toString();
+ });
+ // in close event we are sure that stream from child process is closed
+ python.on('close', (code) => {
+ console.log(`child process close all stdio with code ${code}`);
+ // send data to browser
+ res.send(dataToSend)
+ });
+ 
+})
+app.listen(port, () => console.log(`Example app listening on port 
+${port}!`))
