a9fd1360ff12d363a0c2a92355d56eb2a19eecf7
Nestor Torres <ntnanjuan062@gmail.com>
2021-01-09 19:28:37 -0500

    is done and working

--- Node-App/mainTaskApp/nodetest1/routes/index.js
+++ Node-App/mainTaskApp/nodetest1/routes/index.js
@@ -15,18 +15,19 @@ completion as completion,
 status as status
 FROM TaskList`;
 
-let db = new sqlite3.Database('./data/db/dataStored.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected it working database.');
-});
+// let db = new sqlite3.Database('./data/db/dataStored.db', sqlite3.OPEN_READWRITE, (err) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+//   console.log('Connected it working database.');
+// });
 
 
-db.all(sql, arrayOfTasks, (err, rows) => {
-  if (err) {
-    console.error(err.message);
-  }
+// db.all(sql, arrayOfTasks, (err, rows) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+// });
   // console.log(rows[1].title)
   // console.log(rows[3].title)
 
@@ -34,43 +35,78 @@ db.all(sql, arrayOfTasks, (err, rows) => {
   // console.log(dataStored)
 
   router.get('/', function(req, res, next) {
-    res.render('index', {
-      rows:rows,
-      dataToSend:dataToSend,
+    let db = new sqlite3.Database('./data/db/dataStored.db', sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Connected it working database.');
+    });
+    db.all(sql, arrayOfTasks, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      }
+      // console.log(sql)
+      res.render('index', {
+        rows:rows,
+        dataToSend:dataToSend,
+      });
+    });
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Close the database connection.');
     });
   });
   
   router.post('/update', function(req, res) {
- 
-    var dataToSend;
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-
-    // spawn new child process to call the python script
-    const python = spawn('python', ['./pythonScripts/script1.py', firstName, lastName]);
-    
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
+    let db = new sqlite3.Database('./data/db/dataStored.db', sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Connected it working database.');
     });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        //  res.send(dataToSend)
-        res.render('index', {
-            dataToSend:dataToSend,
-            rows:rows,
-        });
-    }); 
+    db.all(sql, arrayOfTasks, (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      }
+
+      var dataToSend;
+      var firstName = req.body.firstName;
+      var lastName = req.body.lastName;
+  
+      // spawn new child process to call the python script
+      const python = spawn('python', ['./pythonScripts/script1.py', firstName, lastName]);
+      
+      // collect data from script
+      python.stdout.on('data', function (data) {
+          console.log('Pipe data from python script ...');
+          dataToSend = data.toString();
+      });
+      // in close event we are sure that stream from child process is closed
+      python.on('close', (code) => {
+          console.log(`child process close all stdio with code ${code}`);
+          // send data to browser
+          //  res.send(dataToSend)
+          res.render('index', {
+              dataToSend:dataToSend,
+              rows:rows,
+          });
+      }); 
+    });
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Close the database connection.');
+    });
   });
   // router.get('/edit', function(req, res, next) {
   //   res.redirect('edit', {
   //     rows:rows,
   //   });
   // });
-});
+// });
 
 // db.close((err) => {
 //   if (err) {
